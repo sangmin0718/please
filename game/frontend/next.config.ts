1b42ca48add26a4e1ed23ef1a3d07505636aa915
unknown <steve102878@gmail.com>
2025-12-11 13:06:13 +0900

    정적사이트 변환

--- game/frontend/next.config.ts
+++ game/frontend/next.config.ts
@@ -1,7 +1,11 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // (정적 사이트 변환)
+  images: {
+    unoptimized: true, // 이미지 최적화 기능 끄기 (에러 방지용)
+  }
 };
 
 export default nextConfig;
