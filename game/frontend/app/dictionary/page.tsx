3669c8d8e7b5e73876b5454b0d891905a2968652
unknown <steve102878@gmail.com>
2025-12-18 05:26:55 +0900

    딕셔너리 없는곳에 커뮤니티로 변경

--- game/frontend/app/dictionary/page.tsx
+++ game/frontend/app/dictionary/page.tsx
@@ -1,8 +1,6 @@
+// app/dictionary/page.tsx
+import { redirect } from "next/navigation";
+
 export default function DictionaryPage() {
-    
-  return (
-    <div className="w-screen h-screen bg-black flex items-center justify-center">
-      <h1 className="text-white text-4xl">Dictionary Screen</h1>
-    </div>
-  );
+  redirect("https://please-community-frontend.onrender.com");
 }
